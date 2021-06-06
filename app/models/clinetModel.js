7de1969af6cdfd6f6a844150d015c3a647637590
Igor <iluiz.sousa@gmail.com>
2021-06-06 16:32:46 -0300

    adicionando mysql module e establecendo conexao com banco

--- app/models/clinetModel.js
+++ app/models/clinetModel.js
@@ -1,10 +1,27 @@
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'B8sZFG7Hn@',
+    database: 'nodelab_db'
+});
+
+
 module.exports = function (){
+
+    
     this.all = function(){
+
+        conn.query('select * from Persons',function(erro,resultado){
+            console.log(resultado);
+        });
+
         return [{
             nome: "Igor", 
             email: "igoruser@bol.com"
         }];
     };
 
-    return this;
+    return this ;
 };
